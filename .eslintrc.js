cca241f168efda6ce7ffe2377f92c3108a986b05
Ihar Bandarenka <Ihar.Bandarenka@itechart-group.com>
2021-11-01 10:13:11 +0300

    feat:fix eslint/prettier

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,20 +1,44 @@
 module.exports = {
   root: true,
+
   env: {
     node: true,
   },
+
   extends: [
     "plugin:vue/vue3-recommended",
+    "plugin:prettier/recommended",
     "eslint:recommended",
     "@vue/typescript/recommended",
-    "@vue/prettier",
-    "@vue/prettier/@typescript-eslint",
+    // "@vue/prettier",
+    // "@vue/prettier/@typescript-eslint",
   ],
+
   parserOptions: {
     ecmaVersion: 2020,
   },
+
+  // 'extends': [
+  //   'plugin:vue/vue3-recommended',
+  //   'eslint:recommended',
+  //   '@vue/typescript/recommended',
+  //   '@vue/prettier',
+  //   '@vue/prettier/@typescript-eslint',
+  //   'plugin:vue/vue3-essential',
+  //   '@vue/standard'
+  // ]
   rules: {
-    "no-console": process.env.NODE_ENV === "production" ? "warn" : "off",
-    "no-debugger": process.env.NODE_ENV === "production" ? "warn" : "off",
+    'no-console': process.env.NODE_ENV === 'production' ? 'warn' : 'off',
+    'no-debugger': process.env.NODE_ENV === 'production' ? 'warn' : 'off'
   },
+
+  'extends': [
+    'plugin:vue/vue3-recommended',
+    'plugin:prettier/recommended',
+    'eslint:recommended',
+    '@vue/typescript/recommended',
+    'plugin:vue/vue3-essential',
+    '@vue/prettier',
+    '@vue/prettier/@typescript-eslint'
+  ]
 };
